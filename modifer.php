2959576be4bee57bc401999b251b06f46e0176ec
roehan-abdulrauf <91737334+roehan-abdulrauf@users.noreply.github.com>
2021-12-15 18:22:28 +0100

    change 1

--- modifer.php
+++ modifer.php
@@ -1,16 +1,28 @@
 <?php
 session_start();
-
-$bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
-
 if (isset($_SESSION['login'])) {
 
-    $userid = $_GET['id'];
-    var_dump($_GET);
+    $bdd = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
 
-    $req = $bdd->query("UPDATE `utilisateurs` SET login='$_POST[login]', password= '$_POST[login]', email = '$_POST[email]' WHERE id = '$userid'");
-    $req->execute(array());
-    $rows = $req->fetch();
+    if (!$bdd) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    if ($droit ==  $droit2) {
+
+        if (isset($_POST['modifier'])) {
+
+            $userid = $_GET['id'];
+            var_dump($userid);
+
+            $droit = $_POST['droit'];
+            $droit2 = $_POST['droit2'];
+
+            $req = $bdd->query("UPDATE utilisateurs SET id_droits = '$droit' WHERE id = '$userid'");
+            $req->execute(array());
+            $rows = $req->fetch();
+        }
+    }
 }
 
 ?>
@@ -29,38 +41,20 @@ if (isset($_SESSION['login'])) {
     <main>
         <div align="center">
             <div>
-                <h1 class="h1-">Vous etes sur le point de modifier les informations de l'utilisateurs <?php echo $userlogin; ?></h1>
+                <h1 class="h1-">Vous etes sur le point de modifier les informations de l'utilisateurs <? echo "$userid"; ?></h1>
                 <form class="form" method="POST">
                     <table class="form-inpute">
                         <tr>
                         <tr>
                             <td>
-                                <label>Identifiant</label>
-                                <input type="text" name="login" id="newlogin" value="<?php echo $_Get['login']; ?>" />
+                                <label>Nouveau droits</label>
+                                <input type="text" name="droit" id="droit" />
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <label>Nom</label>
-                                <input type="text" name="login" id="newlogin" value="<?php echo $_Get['login']; ?>" />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Ancien mot de passe</label>
-                                <input type="password" name="password" id="password" placeholder="Ancien mot de passe" minlength="8" required />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Nouveau mot de passe</label>
-                                <input type="password" name="newmdp" id="newmdp placeholder=" Nouveau mot de passe" required />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label>Confirmer mot de passe</label>
-                                <input type="password" name="newmdp2" id="newmdp2 placeholder=" Confirmer mot de passe" required />
+                                <label>Confirmer droits</label>
+                                <input type="text" name="droit2" id="droit2" required />
                             </td>
                         </tr>
                         <tr>
@@ -70,15 +64,7 @@ if (isset($_SESSION['login'])) {
                         </tr>
                     </table>
                 </form>
-                <?php if (isset($changelogin)) { ?>
-                    <p> <?php echo $changelogin; ?> </p>
-
-                <?php } elseif (isset($changemdp)) { ?>
-
-                    <p> <?php echo $changemdp; ?> </p>
-
-                <?php } ?>
-
+            </div>
     </main>
 </body>
 
--- suppimer.php
+++ suppimer.php
@@ -8,9 +8,8 @@ if (isset($_SESSION['login'])) {
     $userid = $_GET['id'];
     var_dump($_GET);
 
-    $req = $bdd->query("DELETE * FROM utilisateurs where id = '$userid'");
-    $req->execute(array( $userid));
-    $rows = $req->fetch();
+    $req = $bdd->query("DELETE FROM utilisateurs where id = '$userid'");
+    $req->execute(array());
     header("Location: admin.php");
 }
 
