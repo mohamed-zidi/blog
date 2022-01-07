fb1878ad47874855221d717da429b8eea08a0680
khellaf-rachedi <91748769+khellaf-rachedi@users.noreply.github.com>
2022-01-07 19:16:01 +0100

    Correction page articles

--- traitement_php/articles-traitement.php
+++ traitement_php/articles-traitement.php
@@ -3,25 +3,22 @@
 include '../connexion_bdd/connexion-bdd.php';
 //  $db = new PDO('mysql:host=localhost;dbname=blog','root','');
 
-$perPage = 5;
 $req = $bdd->query('SELECT COUNT(*) AS total FROM articles');
 $result = $req->fetch();
 $total = $result['total'];
 
-$nbPage = ceil($total/$perPage);
-
-$reqarticles = $bdd->query("SELECT article FROM articles ORDER BY id DESC LIMIT 0,5");
-$reqarticles->execute();
 // $reqarticles->fetchAll() ;
 
-if(isset($_GET['p']) && !empty($_GET['p']) && ctype_digit($_GET['p']) ==1)
+if(isset($_GET['p']))
 {
-   if($_GET['p'] > $nbPage){
+    $current = $_GET['p'];
+
+   /* if($_GET['p'] > $nbPage){
        $current = $nbPage;
    }
    else{
        $current = $_GET['p'];
-   }
+   } */
 }
 else
 {
@@ -29,5 +26,14 @@ else
 }
 
 // return $reqarticles = $db->query("SELECT * FROM articles ORDER BY id DESC ");
+
+$perPage = 5 * $current;
+$pageStart = $perPage - 5;
+$pageEnd = $pageStart + 5;
+
+$nbPage = ceil($total/$perPage);
+
+$reqarticles = $bdd->query("SELECT article FROM articles ORDER BY id DESC LIMIT $pageStart, $pageEnd");
+$reqarticles->execute();
 
 ?>
